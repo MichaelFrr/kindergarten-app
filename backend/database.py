688ccaafb3c25d201844ecb3850a4ad6d0f7361d
Michael <yassenmana1@gmail.com>
2026-07-21 15:17:44 +0300

    Added Enviroment Variables

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+script_dir = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=script_dir / ".env")
+
+db_url = os.environ.get("DATABASE_URL")
 
 
-DATABASE_URL = "postgresql://postgres:0@localhost:5432/kindergarten_db"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 from pwdlib import PasswordHash
+import jwt
+
+
+script_dir = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=script_dir / ".env")
+
+Secret_key = os.environ.get("SECRET_KEY")
+
 
 pass_hash = PasswordHash.recommended()
