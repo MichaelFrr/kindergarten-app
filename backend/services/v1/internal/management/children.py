dbca22ea272b7d74c714bcf94de96c1bcddb9c4e
MichaelFrr <yassenmana1@gmail.com>
2026-08-04 16:42:20 +0300

    Add The Logic For The Rest Of The Endpoints

--- backend/services/v1/internal/management/children.py
+++ backend/services/v1/internal/management/children.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from backend.models import Child, User, Classroom
 from backend.schemas import ChildCreate, ChildResponse, ChildUpdate
-from backend.database import getdb, Session, Select
+from backend.database import getdb, Session, Select, or_
 from fastapi import APIRouter, Depends, HTTPException
 router = APIRouter(tags=["Child_V1"])
+from uuid import UUID
 
 
 @router.post("/create_child")
@@ -27,12 +28,29 @@ def create_child(child: ChildCreate, db: Session = Depends(getdb)):
     return new_child
 
 @router.patch("/update_child")
-def update_child(child_data:ChildUpdate, db: Session = Depends(getdb)):
-    pass
+def update_child(child_data:ChildUpdate,parent_email:str, db: Session = Depends(getdb)):
+    child = db.scalars(Select(Child).where(Child.parent_email == parent_email)).first()
+    if not child:
+        raise HTTPException(status_code=404, detail="Child Does Not Exist")
+    update_dict = child_data.model_dump(exclude_unset=True)
+    for key, value in update_dict.items():
+        setattr(child, key, value)
+    db.commit()
+    db.refresh(child)
+    return child
 
-@router.get("child/{classroom}")
-def get_child(data: str, db: Session = Depends(getdb)):
-    pass
+# either add a check here to auto change the classroom capacity or an endpoint specefically to assign a child to a classroom
+
+@router.get("/child/{data}")
+def get_child(data, db: Session = Depends(getdb)):
+    try:
+        uuid_data = UUID(data)
+    except (ValueError, TypeError):
+        uuid_data = None
+
+    query = Select(Child).where(or_(Child.parent_email == data, Child.uuid == uuid_data ))
+    result = db.scalars(query).first()
+    return result
 
 @router.get("/children")
 def get_children(db: Session = Depends(getdb)):
